7172c8459f40f80bb4289bbded64371a88bd617b
Matthew Hasman <90156617+matthewhasman@users.noreply.github.com>
2024-03-08 16:41:12 -0800

    Add files via upload

---
+++ Scripts/Example Code/pynput_example.py
@@ -0,0 +1,23 @@
+# Example code for collecting user inputs
+
+from pynput import keyboard
+from pynput.keyboard import Key
+import time
+
+print("press key")
+def on_press(key):
+    try:
+        if key == keyboard.Key.left:
+            print("You pressed left arrow")
+            print("Continuing execution...")
+            time.sleep(1)
+        elif key == keyboard.Key.right:
+            print("You pressed right arrow")
+            print("Continuing execution...")
+            time.sleep(1)
+    except AttributeError:
+        pass
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
