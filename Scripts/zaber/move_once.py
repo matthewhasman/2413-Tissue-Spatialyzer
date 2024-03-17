e9a8569f305636372fb6939edf386203c61d290c
Cole Rowell <cxlerxwell@gmail.com>
2024-03-16 18:24:03 -0700

    Started GUI

---
+++ Scripts/zaber/move_once.py
@@ -0,0 +1,94 @@
+from zaber_motion import Units
+from zaber_motion.ascii import Connection
+from PyQt6.QtWidgets import *
+import sys
+
+# Constants
+WELL_SPACING_MM = 8.99
+
+def move_once(well="A1", port="COM1", ref_x_coord = 0, ref_y_coord = 200):
+    """Moves to the given well based off of A1 reference x and y coordinates by connection to the Zaber stage with the input port.
+
+    Args:
+        well (string): Desired well to move to.
+        port (string): Port that Zaber stage is connected to.
+        ref_x_coord (int): X coordinate of the A1 well.
+        ref_y_coord (int): Y coordinate of the A1 well.
+    """
+
+    with Connection.open_serial_port(port) as connection:
+        connection.enable_alerts()
+
+        device_list = connection.detect_devices()
+        print("Found {} devices".format(len(device_list)))
+
+        device = device_list[0]
+
+        x_axis = device.get_lockstep(1)
+        y_axis = device.get_axis(3)
+        z_axis = device.get_axis(4)
+
+        x_axis.home()
+        y_axis.home()
+
+        letter = well[0].lower()
+        character_id = ord(letter) - ord('a')
+        number = int(well[1])
+
+        x_coord = ref_x_coord + WELL_SPACING_MM * character_id
+        y_coord = ref_y_coord - WELL_SPACING_MM * (number-1)
+
+        # Remove seal before moving to new well
+        z_axis.move_absolute(100, Units.LENGTH_MILLIMETRES, wait_until_idle=False)
+
+        try:
+            x_axis.move_absolute(x_coord, Units.LENGTH_MILLIMETRES, wait_until_idle=False)
+            y_axis.move_absolute(y_coord, Units.LENGTH_MILLIMETRES, wait_until_idle=False)
+            x_axis.wait_until_idle()
+            y_axis.wait_until_idle()
+            z_axis.wait_until_idle()
+        except Exception as e:
+                #If an exception is raised show the user the exception
+                
+                dialog = QDialog()
+                dialog.setWindowTitle(type(e).__name__)
+
+                layout = QVBoxLayout()
+
+                label = QLabel(str(e))
+                layout.addWidget(label)
+
+                ok_button = QPushButton('OK')
+                ok_button.clicked.connect(dialog.accept)
+                layout.addWidget(ok_button)
+
+                dialog.setLayout(layout)
+                dialog.exec()
+   
+
+# If script is called from terminal check for optional arguments and set default values
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        well = sys.argv[1]
+    else:
+        well = "A1"
+    
+    if len(sys.argv) > 2:
+        port = sys.argv[2]
+    else:
+        port = "COM1"
+
+    if len(sys.argv) > 3:
+        ref_x_coord = int(sys.argv[3])
+    else:
+        ref_x_coord = 0
+    
+    if len(sys.argv) > 4:
+        ref_y_coord = int(sys.argv[4])
+    else:
+        ref_y_coord = 200
+
+    move_once(well, port, ref_x_coord, ref_y_coord)
+
+
+        
---
+++ Scripts/zaber/move_to_well.py
@@ -0,0 +1,122 @@
+import sys
+from move_once import move_once
+from PyQt6.QtWidgets import *
+from zaber_motion import Units
+from zaber_motion.ascii import Connection
+
+WELL_SPACING_mm = 8.99
+
+class WellPlateGUI(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initGUI()
+        self.__x_coord = None
+        self.__y_coord = None
+        self.selected_port = None
+
+    def well_button_clicked(self, button_name):
+        
+        if (self.__x_coord == None or self.__y_coord == None):
+            dialog = QDialog()
+            dialog.setWindowTitle('Warning')
+
+            layout = QVBoxLayout()
+
+            label = QLabel('You must set both the x and y coordinates of the top left well before moving the stage.')
+            layout.addWidget(label)
+
+            ok_button = QPushButton('OK')
+            ok_button.clicked.connect(dialog.accept)
+            layout.addWidget(ok_button)
+
+            dialog.setLayout(layout)
+            dialog.exec()
+        elif (self.selected_port == None):
+            dialog = QDialog()
+            dialog.setWindowTitle('Warning')
+
+            layout = QVBoxLayout()
+
+            label = QLabel('You must selected a serial port before using the stage.')
+            layout.addWidget(label)
+
+            ok_button = QPushButton('OK')
+            ok_button.clicked.connect(dialog.accept)
+            layout.addWidget(ok_button)
+
+            dialog.setLayout(layout)
+            dialog.exec()
+        else:
+            move_once(button_name, self.selected_port)
+
+
+    def handle_x_input(self, line_edit):
+        self.__x_coord = line_edit.text()
+
+    def handle_y_input(self, line_edit):
+        self.__y_coord = line_edit.text()
+
+    def selectionChanged(self, index):
+        if index != 0:
+            self.selected_port = "COM" + str(index)
+        else:
+            self.selected_port = None
+
+    def initGUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        # Add labels for rows (A-H)
+        rows_label = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+        # Add labels for columns (1-12)
+        columns_label = [str(i) for i in range(1, 13)]
+
+        # Add buttons for each well
+        for row in range(len(rows_label)):
+            for col in range(len(columns_label)):
+                button_name = f'{rows_label[row]}{columns_label[col]}'
+                button = QPushButton(button_name)
+                button.clicked.connect(lambda checked, name=button_name: self.well_button_clicked(name))
+                grid.addWidget(button, row, col)
+
+
+        x_line_edit = QLineEdit()
+        x_line_edit.returnPressed.connect(lambda: self.handle_x_input(x_line_edit))
+        grid.addWidget(x_line_edit, 1, 13)
+
+        y_line_edit = QLineEdit()
+        y_line_edit.returnPressed.connect(lambda: self.handle_y_input(y_line_edit))
+        grid.addWidget(y_line_edit, 1, 14)
+
+        # Add label for the QLineEdit
+        label = QLabel('Top Left X Position:')
+        grid.layout().addWidget(label, 0, 13)
+
+        label = QLabel('Top Left Y Position:')
+        grid.layout().addWidget(label, 0, 14)
+
+        comboBox = QComboBox()
+        comboBox.addItem("")
+        comboBox.addItem("COM1")
+        comboBox.addItem("COM2")
+        comboBox.addItem("COM3")
+        comboBox.addItem("COM4")
+        comboBox.addItem("COM5")
+        comboBox.addItem("COM6")
+        comboBox.currentIndexChanged.connect(self.selectionChanged)
+        grid.layout().addWidget(comboBox, 3, 13)
+
+        label = QLabel('Port Selection')
+        grid.layout().addWidget(label, 2, 13)
+
+        self.setWindowTitle('96-Well Microplate GUI')
+        self.setGeometry(100, 100, 800, 600)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    well_plate_gui = WellPlateGUI()
+    app.exec()
