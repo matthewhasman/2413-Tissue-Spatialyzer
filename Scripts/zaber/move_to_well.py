b60b641b0718e5649852582e1006ae3b26ac683e
Cole Rowell <cxlerxwell@gmail.com>
2024-03-18 14:55:47 -0700

    Editied move to well GUI

--- Scripts/zaber/move_to_well.py
+++ Scripts/zaber/move_to_well.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt6.QtGui import QCloseEvent
 from move_once import move_once
 from create_seal import create_seal
 from PyQt6.QtWidgets import *
@@ -49,19 +51,67 @@ class WellPlateGUI(QWidget):
             dialog.setLayout(layout)
             dialog.exec()
         else:
-            move_once(button_name, self.selected_port)
+            letter = button_name[0].lower()
+            character_id = ord(letter) - ord('a')
+            number = int(button_name[1])
+
+            x_coord = self.x_coord + WELL_SPACING_mm * character_id
+            y_coord = self.y_coord - WELL_SPACING_mm * (number-1)
+
+            self.x_axis.wait_until_idle()
+            self.y_axis.wait_until_idle()
+            self.z_axis.wait_until_idle()
+
+            # Remove seal before moving to new well
+            self.z_axis.move_absolute(50, Units.LENGTH_MILLIMETRES, wait_until_idle=False)
+
+            try:
+                self.x_axis.move_absolute(x_coord, Units.LENGTH_MILLIMETRES, wait_until_idle=False)
+                self.y_axis.move_absolute(y_coord, Units.LENGTH_MILLIMETRES, wait_until_idle=False)
+                self.x_axis.wait_until_idle()
+                self.y_axis.wait_until_idle()
+                self.z_axis.wait_until_idle()
+            except:
+                dialog = QDialog()
+                dialog.setWindowTitle(type(e).__name__)
+
+                layout = QVBoxLayout()
+
+                label = QLabel(str(e))
+                layout.addWidget(label)
+
+                ok_button = QPushButton('OK')
+                ok_button.clicked.connect(dialog.accept)
+                layout.addWidget(ok_button)
+
+                dialog.setLayout(layout)
+                dialog.exec()
 
 
     def handle_x_input(self, line_edit):
-        self.x_coord = line_edit.text()
+        self.x_coord = int(line_edit.text())
 
     def handle_y_input(self, line_edit):
-        self.y_coord = line_edit.text()
+        self.y_coord = int(line_edit.text())
 
     def selectionChanged(self, index):
         if index != 0:
             self.selected_port = "COM" + str(index)
+            self.connection = Connection.open_serial_port(self.selected_port)
+            self.connection.enable_alerts()
+
+            device_list = self.connection.detect_devices()
+            device = device_list[0]
+
+            self.x_axis = device.get_lockstep(1)
+            self.y_axis = device.get_axis(3)
+            self.z_axis = device.get_axis(4)
+
+            self.x_axis.home(wait_until_idle=False)
+            self.y_axis.home(wait_until_idle=False)
+            self.z_axis.home(wait_until_idle=False)
         else:
+            self.connection.close()
             self.selected_port = None
 
     def test_connection(self):
@@ -172,6 +222,10 @@ class WellPlateGUI(QWidget):
         self.setWindowTitle('96-Well Microplate GUI')
         self.setGeometry(100, 100, 800, 600)
         self.show()
+
+    def closeEvent(self, event):
+        self.connection.close()
+        return super().closeEvent(event)
 
 
 if __name__ == '__main__':
